9beb6d02cadd2d7293e5853d0ba307846bbf94d1
danmed1 <ldaniel.med@gmail.com>
2015-04-30 00:53:47 -0500

    no message

--- Proyecto2/src/proyecto2/Arbol.java
+++ Proyecto2/src/proyecto2/Arbol.java
@@ -14,37 +14,21 @@ import java.util.HashMap;
  */
 public class Arbol {
 
-    private String[] alfabeto;
-    private Simbolo inicial;
     private HashMap<String, Simbolo> noTerminales;
     private NodoArbol raiz;
 
-    public Arbol(String[] alfabeto, Simbolo inicial) {
-        this.alfabeto = alfabeto;
-        this.inicial = inicial;
-        raiz = new NodoArbol(inicial.getCaracter()+"");
+    public Arbol(String inicial) {
+        raiz = new NodoArbol(inicial);
         noTerminales = new HashMap<>();
-    }
-
-    public String[] getAlfabeto() {
-        return alfabeto;
-    }
-
-    public void setAlfabeto(String[] alfabeto) {
-        this.alfabeto = alfabeto;
-    }
-
-    public Simbolo getInicial() {
-        return inicial;
-    }
-
-    public void setInicial(Simbolo inicial) {
-        this.inicial = inicial;
     }
 
     public void addNoTerminal(String str, Simbolo s) {
         noTerminales.put(str, s);
     }
+
+    public HashMap<String, Simbolo> getNoTerminales() {
+        return noTerminales;
+    }    
 
     private ArrayList<NodoArbol> generaHijos(NodoArbol nodo) {
         String str = nodo.getContenido();
@@ -54,7 +38,7 @@ public class Arbol {
             nuevo = new ArrayList<>();
             Simbolo aux = noTerminales.get((str.charAt(index) + ""));
             for (int i = 0; i < aux.getProduccion().size(); i++) {
-                nuevo.add(new NodoArbol(str.substring(0, index) + noTerminales.get((str.charAt(index) + "")).getProduccion().get(i) + str.substring(index + 1, str.length())));
+                nuevo.add(new NodoArbol(str.substring(0, index) + aux.getProduccion().get(i) + str.substring(index + 1, str.length())));
             }
         }
         nodo.setHijos(nuevo);
@@ -76,40 +60,41 @@ public class Arbol {
 
     public boolean analize(String input, int profundidad) {
         NodoArbol nod = raiz;
-        ArrayList<NodoArbol> nodosEnNivel= new ArrayList<>();
-        ArrayList<NodoArbol> nuevoNivel= new ArrayList<>();
-        boolean ret=false;
+        ArrayList<NodoArbol> nodosEnNivel = new ArrayList<>();
+        ArrayList<NodoArbol> nuevoNivel = new ArrayList<>();
+        boolean ret = false;
         int n = profundidad;
         nodosEnNivel.add(nod);
         while (n > 0) {
-            for(NodoArbol aux: nodosEnNivel){
+            for (NodoArbol aux : nodosEnNivel) {
                 nuevoNivel.addAll(generaHijos(aux));
             }
-            if(!nuevoNivel.contains(new NodoArbol(input))){
+            if (!nuevoNivel.contains(new NodoArbol(input))) {
                 nodosEnNivel.clear();
                 nodosEnNivel.addAll(nuevoNivel);
                 nuevoNivel.clear();
-            }else{
-                ret=true;
-                n=0;
+            } else {
+                ret = true;
+                n = 0;
             }
             n--;
         }
         return ret;
     }
-    public String print (){
+
+    public String print() {
         return treeString(raiz, 4);
     }
-    
-    private String treeString(NodoArbol nodo, int sp){
-        String ret="";
-        if(nodo.getHijos()!=null && nodo.getHijos().isEmpty()){
-            ret+=String.format("%1$" + sp + "s", "["+nodo.getContenido()+"]\n");
-        }else{
-            for(NodoArbol aux: nodo.getHijos()){
-                ret=ret+treeString(aux, sp+4);
+
+    private String treeString(NodoArbol nodo, int sp) {
+        String ret = "";
+        if (nodo.getHijos() != null && nodo.getHijos().isEmpty()) {
+            ret += String.format("%1$" + sp + "s", "[" + nodo.getContenido() + "]\n");
+        } else {
+            for (NodoArbol aux : nodo.getHijos()) {
+                ret = ret + treeString(aux, sp + 4);
             }
-            ret=String.format("%1$" + sp + "s", "["+nodo.getContenido()+"]\n")+ret;
+            ret = String.format("%1$" + sp + "s", "[" + nodo.getContenido() + "]\n") + ret;
         }
         return ret;
     }
--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -5,36 +5,86 @@
  */
 package proyecto2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author Dan
  */
 public class Proyecto2 {
 
+    private static ArrayList<Simbolo> terminales = new ArrayList<>();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Simbolo in = new Simbolo('S');
-        in.insertProduction("aA");
-        in.insertProduction("bB");
-        Simbolo sa = new Simbolo('A');
-        sa.insertProduction("a");
-        Simbolo sb = new Simbolo('B');
-        sb.insertProduction("b");
-        sb.insertProduction("bB");
-        
-        String[] alfabeto = {"a","b"};
-        
-        Arbol tree = new Arbol(alfabeto,in);
-        tree.addNoTerminal(in.toString(), in);
-        tree.addNoTerminal(sa.toString(), sa);
-        tree.addNoTerminal(sb.toString(), sb);
-        
-        System.out.println(tree.analize("bbb", 8));
-        System.out.println(tree.print());
-        // TODO code application logic here
+
+        File list = new File("prueba.txt");
+        try {
+            FileReader fileReader = new FileReader(list);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+
+            //Inicialización de Gramatica
+            line = reader.readLine();
+            String[] noTerminales = line.split(",");
+            ArrayList<Simbolo> gramatica = new ArrayList<>();
+            for (String aux : noTerminales) {
+                Simbolo tem = new Simbolo(aux.charAt(0));
+                gramatica.add(tem);
+            }
+
+            //Arreglo de Simbolos Terminales
+            line = reader.readLine();
+            String[] sTerminales = line.split(",");
+            for (String aux : sTerminales) {
+                Simbolo tem = new Simbolo(aux.charAt(0));
+                terminales.add(tem);
+            }
+
+            //Inicializa árbol
+            line = reader.readLine();
+            Arbol tree = new Arbol(line);
+            for (Simbolo aux : gramatica) {
+                tree.addNoTerminal(aux.getCaracter() + "", aux);
+            }
+
+            //Llena la gramatica
+            while ((line = reader.readLine()) != null) {
+                String[] produccion = line.split("->");
+
+                gramatica.get(gramatica.indexOf(produccion[0])).getProduccion().add(produccion[1]);
+            }
+
+            Simbolo in = new Simbolo('S');
+            in.insertProduction("aA");
+            in.insertProduction("bB");
+            Simbolo sa = new Simbolo('A');
+            sa.insertProduction("a");
+            Simbolo sb = new Simbolo('B');
+            sb.insertProduction("b");
+            sb.insertProduction("bB");
+
+            tree.addNoTerminal(in.toString(), in);
+            tree.addNoTerminal(sa.toString(), sa);
+            tree.addNoTerminal(sb.toString(), sb);
+
+            System.out.println("Gramatica");
+            for (Simbolo aux : tree.getNoTerminales().values()) {
+                System.out.println(aux);
+            }
+            System.out.println("Gramatica");
+            System.out.println(tree.print());
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
